e61c5d44e4dd715563cdeb35fd377f80ed2d869f
szutryk <wojtekszutryk@gmail.com>
2021-06-29 13:53:52 +0200

    fetch dog breeds from api, add changing image on click

--- js/app.js
+++ js/app.js
@@ -3,17 +3,25 @@ import '../sass/style.scss';
 class Dog {
     constructor() {
         this.apiURL = 'https://dog.ceo/api';
-        this.imgElement = document.querySelectorAll('.dog-details__image img');
-
+        this.breedsUl = document.querySelector('.dog-details__list');
+        this.search = document.querySelector('.header-container input');
         this.init();
     }
-    async listAllBreads(){
-        return await fetch(`${this.apiURL}/breeds/list/all`)
+    async listBreads(){
+        const breedList = [];
+        const breeds = await fetch(`${this.apiURL}/breeds/list/all`)
             .then(response => response.json())
-            .then(data => {return data.message})
+            .then(data => {return data.message});
+        for (const [breed, subBreeds] of Object.entries(breeds)){
+            if (subBreeds.length === 0) breedList.push(breed)
+            else {
+                for (const subBreed of subBreeds) breedList.push(breed + ' ' + subBreed)
+            }
+        }
+        return breedList;
     };
-    getRandomImage(){
-        return fetch(`${this.apiURL}/breeds/image/random`)
+    async getRandomImage(){
+        return await fetch(`${this.apiURL}/breeds/image/random`)
             .then(response => response.json())
             .then(data => {return data.message})
     };
@@ -22,14 +30,33 @@ class Dog {
             .then(response => response.json())
             .then(data => {return data.message})
     };
+    addBreedToDOM(breed){
+        let breedUrl = breed;
+        if (breedUrl.includes(' ')) breedUrl = breed.replace(' ', '/')
 
-    init(){
-        [...this.imgElement].forEach(element => {
-            this.getRandomImage()
-                .then(src => element.setAttribute('src', src));
-        })
-        this.listAllBreads()
-            .then(breeds => console.log(breeds))
+        const li = document.createElement('li');
+        li.className = 'dog-details__image';
+
+        const img = document.createElement('img');
+        this.getRandomImageByBreed(breedUrl).then(src => img.setAttribute('src', src));
+
+        const p = document.createElement('p');
+        p.className = 'dog-details__description';
+        p.textContent = breed;
+
+        li.appendChild(img);
+        li.appendChild(p);
+
+        li.addEventListener('click', ()=>{
+            this.getRandomImageByBreed(breedUrl).then(src => img.setAttribute('src', src));
+        });
+
+        this.breedsUl.appendChild(li);
+    }
+
+    async init(){
+        const list = await this.listBreads();
+        list.forEach(breed => this.addBreedToDOM(breed));
     }
 }
 
